dfc1fdefacbeeb7665e3f4cb1e725990fed45e61
Jh0N D4t3 <crazy-jho@hush.com>
2015-08-06 16:37:53 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+echo "installing..."
+sudo curl --silent https://Prehelios.github.io/explain.sh/explain.sh --output /usr/bin/explain.sh
+sudo chmod +x /usr/bin/explain.sh
+echo "explain.sh installed"
